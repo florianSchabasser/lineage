3ab1de8898b428ea782f3256678f66380b346df3
Florian Schabasser <florian.schabasser@alumni.tuwien.ac.at>
2024-05-19 21:53:02 +0200

    Initial commit

---
+++ src/test/java/at/tuwien/lineage/LineageApplicationTests.java
@@ -0,0 +1,13 @@
+package at.tuwien.lineage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LineageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
