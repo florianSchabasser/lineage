3de52dc7a90132905d17f06d8709ac7b6f4b25f0
Florian Schabasser <florian.schabasser@alumni.tuwien.ac.at>
2024-10-20 09:25:08 +0200

    improved consumer config;

--- src/main/java/at/tuwien/lineage/config/KafkaConfig.java
+++ src/main/java/at/tuwien/lineage/config/KafkaConfig.java
@@ -1,56 +1,95 @@
 package at.tuwien.lineage.config;
 
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.listener.ContainerProperties;
+import org.springframework.kafka.listener.DefaultErrorHandler;
+import org.springframework.util.backoff.FixedBackOff;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @EnableKafka
 @Configuration
 public class KafkaConfig {
 
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
     @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, String> batchFactory(
-            ConsumerFactory<String, String> consumerFactory) {
-        ConcurrentKafkaListenerContainerFactory<String, String> factory =
-                new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(consumerFactory);
+    public ConsumerFactory<String, String> consumerFactory() {
+        Map<String, Object> config = new HashMap<>();
+
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        config.put(ConsumerConfig.GROUP_ID_CONFIG, "lineage-backend");
+        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+
+        config.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
+        config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        config.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, 100000);
+
+        config.put(ConsumerConfig.FETCH_MIN_BYTES_CONFIG, "1048576");
+        config.put(ConsumerConfig.FETCH_MAX_BYTES_CONFIG, "104857600");
+        config.put(ConsumerConfig.FETCH_MAX_WAIT_MS_CONFIG, 1000);
+        config.put(ConsumerConfig.MAX_PARTITION_FETCH_BYTES_CONFIG, "10485760");
+
+        config.put(ConsumerConfig.RECEIVE_BUFFER_CONFIG, "1048576");
+        config.put(ConsumerConfig.SEND_BUFFER_CONFIG, "1048576");
+
+        return new DefaultKafkaConsumerFactory<>(config);
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String> batchFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
         factory.setBatchListener(true);
         factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL_IMMEDIATE);
+        factory.setCommonErrorHandler(getErrorHandler());
         return factory;
     }
 
     @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, String> singleFactory(
-            ConsumerFactory<String, String> consumerFactory) {
-        ConcurrentKafkaListenerContainerFactory<String, String> factory =
-                new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(consumerFactory);
+    public ConcurrentKafkaListenerContainerFactory<String, String> singleFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
         factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL_IMMEDIATE);
+        factory.setCommonErrorHandler(getErrorHandler());
         return factory;
+    }
+
+    private DefaultErrorHandler getErrorHandler() {
+        // 2 retries + 1 original attempt
+        return new DefaultErrorHandler(new FixedBackOff(1000L, 2L));
     }
 
     @Bean
     public NewTopic lineageNodeTopic() {
-        return TopicBuilder
-                .name("lineage-node")
+        return TopicBuilder.name("lineage-node")
                 .partitions(3)
                 .replicas(1)
-                .config(org.apache.kafka.common.config.TopicConfig.CLEANUP_POLICY_CONFIG, org.apache.kafka.common.config.TopicConfig.CLEANUP_POLICY_DELETE)
+                .config(org.apache.kafka.common.config.TopicConfig.CLEANUP_POLICY_CONFIG,
+                        org.apache.kafka.common.config.TopicConfig.CLEANUP_POLICY_DELETE)
                 .build();
     }
 
     @Bean
     public NewTopic lineageFlowTopic() {
-        return TopicBuilder
-                .name("lineage-flow")
+        return TopicBuilder.name("lineage-flow")
                 .partitions(30)
                 .replicas(1)
-                .config(org.apache.kafka.common.config.TopicConfig.CLEANUP_POLICY_CONFIG, org.apache.kafka.common.config.TopicConfig.CLEANUP_POLICY_DELETE)
+                .config(org.apache.kafka.common.config.TopicConfig.CLEANUP_POLICY_CONFIG,
+                        org.apache.kafka.common.config.TopicConfig.CLEANUP_POLICY_DELETE)
                 .build();
     }
 }
